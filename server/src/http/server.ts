aed8121b02861971689d7e60bb620b97db5af775
ludiemert <lucianadiemert@gmail.com>
2024-09-10 20:45:31 -0300

    add route server backend

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,1 +1,17 @@
-console.log('Hello NLW 🥰😘😍')
+import fastify from "fastify"
+
+const app = fastify()
+
+app.listen({
+  port: 3333,
+
+}).then(() => {
+  console.log('HTTP server running!!! 🥰😘😍')
+
+})
+
+
+
+
+
+
