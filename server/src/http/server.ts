511a2540c1d3dda23fbbcd7be21d9ddf461727b0
ludiemert <lucianadiemert@gmail.com>
2024-09-11 17:49:16 -0300

    add db Drizzle

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,17 +1,11 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
-  port: 3333,
-
-}).then(() => {
-  console.log('HTTP server running!!! 🥰😘😍')
-
-})
-
-
-
-
-
-
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!!! 🥰😘😍')
+  })
