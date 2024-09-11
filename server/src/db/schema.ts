c43f5ae6065add6f46fff56c6464f7d5995f780e
ludiemert <lucianadiemert@gmail.com>
2024-09-11 20:32:13 -0300

    criate seed user in the bd

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,8 +1,11 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2"; //cria id unico
 
 //table goals
 export const goals = pgTable("goals", {
-	id: text("id").primaryKey(),
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => createId()),
 	title: text("title").notNull(),
 	desiredWeeklyFrenquency: integer("desired_weekly_frequency").notNull(),
 	createdAt: timestamp("created_at", { withTimezone: true })
@@ -12,7 +15,9 @@ export const goals = pgTable("goals", {
 
 //other table - goal_completions
 export const goalCompletions = pgTable("goal_completions", {
-	id: text("id").primaryKey(),
+	id: text("id")
+		.primaryKey()
+		.$defaultFn(() => createId()),
 	goalId: text("goal_id")
 		.references(() => goals.id)
 		.notNull(),
