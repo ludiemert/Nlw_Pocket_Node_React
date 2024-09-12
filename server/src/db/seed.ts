8dec0b8229fb0ad909a54ae490fdb5004ec31f9c
ludiemert <lucianadiemert@gmail.com>
2024-09-11 21:17:26 -0300

    add lib days and create rout create-goals

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,3 +1,28 @@
-async function seed() {}
+import dayjs from "dayjs";
+import { client, db } from ".";
+import { goalCompletions, goals } from "./schema";
 
-seed();
+async function seed() {
+	await db.delete(goalCompletions);
+	await db.delete(goals);
+
+	const result = await db
+		.insert(goals)
+		.values([
+			{ title: "wake up early", desiredWeeklyFrenquency: 6 },
+			{ title: "exercise me", desiredWeeklyFrenquency: 4 },
+			{ title: "meditate", desiredWeeklyFrenquency: 3 },
+		])
+		.returning();
+
+	const startOfWeek = dayjs().startOf("week");
+
+	await db.insert(goalCompletions).values([
+		{ goalId: result[0].id, createdAt: startOfWeek.toDate() },
+		{ goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
+	]);
+}
+
+seed().finally(() => {
+	client.end();
+});
