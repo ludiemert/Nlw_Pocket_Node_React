209495171a58e0c0edcdab55aa351e379f05a643
ludiemert <lucianadiemert@gmail.com>
2024-09-11 21:46:43 -0300

    create route goals and test postmam

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,12 +1,28 @@
-import fastify from 'fastify'
-import 'dotenv/config' // Carrega variáveis de ambiente do arquivo .env
+import fastify from "fastify";
+//import 'dotenv/config' // Carrega variáveis de ambiente do arquivo .env
+import z from "zod";
+import { createGoal } from "../functions/create-goal";
 
-const app = fastify()
+const app = fastify();
+
+app.post("/goals", async (request) => {
+	const createGoalSchema = z.object({
+		title: z.string(),
+		desiredWeeklyFrenquency: z.number().int().min(1).max(7),
+	});
+
+	const body = createGoalSchema.parse(request.body);
+
+	await createGoal({
+		title: body.title,
+		desiredWeeklyFrenquency: body.desiredWeeklyFrenquency,
+	});
+});
 
 app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP server running!!! 🥰😘😍')
-  })
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP server running!!! 🥰😘😍");
+	});
