8c618da7491aa4febf80f8f7b270382fc61447e9
ludiemert <lucianadiemert@gmail.com>
2024-09-16 20:34:30 -0300

    config front end  formatar codigo - npm i @biomejs/biome –D and main error

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,3 @@
 export function App() {
-	return <h1>Hello Worlddddd</h1>;
+  return <h1>Hello Worlddddd</h1>
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,16 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+import './index.css'
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-	createRoot(rootElement).render(
-		<StrictMode>
-			<App />
-		</StrictMode>,
-	);
-} else {
-	console.error("Failed to find the root element.");
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
