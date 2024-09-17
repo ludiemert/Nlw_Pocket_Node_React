26fa78741ebb4b22af6ea2c1613d39b251884092
ludiemert <lucianadiemert@gmail.com>
2024-09-17 20:57:49 -0300

    add components project front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,62 @@
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start-illustration.svg'
 
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
+import { Button } from './components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from './components/ui/dialog'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        You haven't registered any goals yet, how about registering one right
-        now?
-      </p>
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight  hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Register Goal 🥳
-      </button>
-    </div>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in.orbit" />
+        <img src={letsStart} alt="in.orbit" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          You haven't registered any goals yet, how about registering one right
+          now?
+        </p>
+
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="size-4" />
+            Register Goal 🥳
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <DialogContent>
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <DialogTitle>Register goal</DialogTitle>
+              <DialogClose>
+                <X className="size-5 text-zinc-600" />
+              </DialogClose>
+            </div>
+
+            <DialogDescription>
+              Add activities that are good for you and that you want to continue
+              practicing every week.
+            </DialogDescription>
+          </div>
+
+          <form action="" className="flex-1 flex flex-col justify-between">
+            <div></div>
+
+            <div className="flex items-center gap-3">
+              <Button>Close</Button>
+              <Button>Save</Button>
+            </div>
+          </form>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
