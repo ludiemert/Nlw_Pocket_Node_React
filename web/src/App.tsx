43b4c66edcba9fe2d371224fdd273190fc8a4e50
ludiemert <lucianadiemert@gmail.com>
2024-09-17 18:52:14 -0300

    add img pag start PAGE

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,14 @@
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+
+
 export function App() {
-  return <h1 className="bg-slate-600 text-center text-white text-4xl">Hello Worlddddd</h1>
+  return (
+		<div className="h-screen flex flex-col items-center justify-center gap-8">
+
+		<img src={logo} alt='in.orbit' />		
+		<img src={letsStart} alt='in.orbit' />
+		
+		</div>
+	)
 }
