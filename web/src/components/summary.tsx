63e01f484eae394e09af2a57080dfc245882121c
ludiemert <lucianadiemert@gmail.com>
2024-09-19 15:24:17 -0300

    ajust Register goal page

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,3 +1,20 @@
+import { Plus } from 'lucide-react'
+import { DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
+
 export function Summary() {
-  return <h1>Summary</h1>
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <span className="text-lg font-semibold">August 5th to 12th</span>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Register Goal 🥳
+          </Button>
+        </DialogTrigger>
+      </div>
+    </div>
+  )
 }
