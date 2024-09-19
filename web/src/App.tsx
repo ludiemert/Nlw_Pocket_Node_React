9dcf8599583af6b8b137e7a684e7ec9fa77291a9
ludiemert <lucianadiemert@gmail.com>
2024-09-19 15:11:04 -0300

    create Register goal page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,16 @@
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-
-import { Plus } from 'lucide-react'
-import { Button } from './components/ui/button'
-import { Dialog, DialogTrigger } from './components/ui/dialog'
+import { Dialog } from './components/ui/dialog'
 
 import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+
+//import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in.orbit" />
-        <img src={letsStart} alt="in.orbit" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          You haven't registered any goals yet, how about registering one right
-          now?
-        </p>
+      {/* <EmptyGoals /> */}
 
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Register Goal 🥳
-          </Button>
-        </DialogTrigger>
-      </div>
-
+      <Summary />
       <CreateGoal />
     </Dialog>
   )
---
+++ web/src/components/empty-goals.tsx
@@ -0,0 +1,27 @@
+import logo from '../assets/logo-in-orbit.svg'
+import letsStart from '../assets/lets-start-illustration.svg'
+
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+
+import { DialogTrigger } from './ui/dialog'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        You haven't registered any goals yet, how about registering one right
+        now?
+      </p>
+
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Register Goal 🥳
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
