239e3da9e683f3c2867ac5cb94edf376bd3966ec
ludiemert <lucianadiemert@gmail.com>
2024-09-20 20:38:41 -0300

    tests REACT and estado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,41 @@
 import { Dialog } from './components/ui/dialog'
 
 import { CreateGoal } from './components/create-goal'
-import { Summary } from './components/summary'
+//import { Summary } from './components/summary'
+import { useState } from 'react'
 
 //import { EmptyGoals } from './components/empty-goals'
 
+//let count = 5
+
 export function App() {
+  const [count, setCount] = useState(5)
+  const [summary, setSumary] = useState(null)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  fetch('http://localhost:3333/summary')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setSumary(data)
+    })
+
   return (
     <Dialog>
       {/* <EmptyGoals /> */}
+      <button type="button" onClick={increment}>
+        Incrementar
+      </button>
 
-      <Summary />
+      <h1 className="text-4xl">{count}</h1>
+
+      <pre>{JSON.stringify(summary, null, 2)}</pre>
+
+      {/*   <Summary /> */}
       <CreateGoal />
     </Dialog>
   )
